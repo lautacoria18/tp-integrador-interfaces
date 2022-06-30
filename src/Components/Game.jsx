928a37526589ca1f31ba1f1d6b6c35522f8cec21
Lautaro Coria <lautarocoria97@gmail.com>
2022-06-30 20:47:18 -0300

    pvp local listo

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -13,11 +13,13 @@ import './Game.css';
 export default function Game() {
 
 
+    //PONER LA JUGADA EN UN RECUADRO ASI EVITO PROBLEMAS DE ESPACIO
+
 
     const plays= ["lizard", "paper", "rock", "scissors", "spock" ];
 
-    const [player1Play, setPlayer1Play] = useState("");
-    const [comPlay, setComPlay] = useState("");
+    const [player1Play, setPlayer1Play] = useState(null);
+    const [comPlay, setComPlay] = useState(null);
     const [timesPlayed, setTimesPlayed] = useState(0);
 
     const [isPlaying, setIsPlaying] = useState(false);
@@ -30,29 +32,43 @@ export default function Game() {
     
     useEffect(() => {
         
-          
+    if (gameMode === "vsCOM")
         if (checkHands(player1Play,comPlay) === player1Play){
-            //setResult("YOU WIN");
-            //setPlayer1Score(player1Score+ 1);
-            setResult("YOU WIN");
+                setResult("YOU WIN");
             setPlayer1Score(player1Score+1);
         }
         else if (checkHands(player1Play,comPlay) === comPlay){
-            //setResult("YOU LOSE");
-            //setPlayer2Score(player2Score + 1);
-            setResult("YOU LOSE");
+                  setResult("YOU LOSE");
             setPlayer2Score(player2Score+1);
             
         }
         else if (checkHands(player1Play,comPlay) === "tie"){
-            //setResult("TIE");
-            setResult("TIE");
+                        setResult("TIE");
         }
         
       }, [player1Play, comPlay, timesPlayed]);
+
+
+      useEffect(() => {
+        
+        if (gameMode === "vsP2")
+            if (checkHands(player1Play,comPlay) === player1Play){
+                    setResult("PLAYER 1 WINS!");
+                setPlayer1Score(player1Score+1);
+            }
+            else if (checkHands(player1Play,comPlay) === comPlay){
+                      setResult("PLAYER 2 WINS!");
+                setPlayer2Score(player2Score+1);
+                
+            }
+            else if (checkHands(player1Play,comPlay) === "tie"){
+                            setResult("TIE");
+            }
+            
+          }, [timesPlayed]);
     
 
-
+    
 
     const checkHands= (p1, p2) => {
 
@@ -93,30 +109,6 @@ export default function Game() {
             return "tie"
         }
     }
-
-    const checkPlay= (p1,p2) => {
-
-       
-        if (checkHands(p1,p2) === p1){
-            //setResult("YOU WIN");
-            //setPlayer1Score(player1Score+ 1);
-            return "YOU WIN"
-        }
-        else if (checkHands(p1,p2) === p2){
-            //setResult("YOU LOSE");
-            //setPlayer2Score(player2Score + 1);
-            return "YOU LOSE"
-            
-        }
-        else if (checkHands(p1,p2) === "tie"){
-            //setResult("TIE");
-            return "TIE"
-        }
-    
-   
-        
-    }
-
     
     const PlaySelected = ({image}) => {
         switch (image) {
@@ -156,60 +148,174 @@ export default function Game() {
         
       }
 
-     
+      const handleClick = (value, isPlayer1) => {
+        if (isPlayer1){
+        setPlayer1Play(value);
+        setComPlay(plays[Math.floor(Math.random() * 5)]);
+        
 
+      }
+      else{
+        setComPlay(value);
+        
+      }
+      setTimesPlayed(timesPlayed +1) 
+    }
+    const handleClickPVP = (value, isPlayer1) => {
+        if (isPlayer1){
+        setPlayer1Play(value);
+                
+
+      }
+      else{
+        setComPlay(value);
+        setTimesPlayed(timesPlayed +1) 
+      }
       
-  return (
-    <div>
-            <div>
-                <h3 className='vsCom' onClick={() => {setGameMode("com"); setIsPlaying(true);}}>VS COM</h3>
-                <h3>VS P2</h3>
-            </div>
-            <div className='videogame'style={{  
-                        display: isPlaying ? 'contents' : 'none',
-      }} >
+    }
+
+const resetValues = () =>{
+    setPlayer1Play(null);
+    setComPlay(null);
+    setResult("");
+
+}
+
+     
+    const againstComputer= () =>{
+return(
+    <div className='videogame' >
+                
                 
                 <div className='versus'>
                 <div className='yourOptions'>
                     <div className='a'>
-                        <h1>Choose your play!</h1>
-                        <div className='images'>
-                        <img src={lizard} alt="lizard" onClick={() => {checkPlay(); console.log("hika")}} />
-                        <img src={paper} alt="lizard" onClick={() => {setPlayer1Play("paper"); setComPlay(plays[Math.floor(Math.random() * 5)]); setTimesPlayed(timesPlayed +1) }} />
-                        <img src={rock} alt="lizard" onClick={() => {{setPlayer1Play("rock")}; setComPlay(plays[Math.floor(Math.random() * 5)])}} />
-                        <img src={scissors} alt="lizard" onClick={() => {{setPlayer1Play("scissors")}; setComPlay(plays[Math.floor(Math.random() * 5)])}} />
-                        <img src={spock} alt="lizard" onClick={() => {{setPlayer1Play("spock")}; setComPlay(plays[Math.floor(Math.random() * 5)])}} />
+                        <h1>Player 1</h1>
+                        <div className='images' style={{  
+            pointerEvents: player1Play != null ? 'none' : 'auto',
+        }}>
+                        <img src={lizard} alt="lizard" onClick={() => {handleClick("lizard", true)}} />
+                        <img src={paper} alt="paper" onClick={() => {handleClick("paper", true)}} />
+                        <img src={rock} alt="rock" onClick={() => {handleClick("rock", true)}}/>
+                        <img src={scissors} alt="scissors" onClick={() => {handleClick("scissors", true)}} />
+                        <img src={spock} alt="spock" onClick={() => {handleClick("spock", true)}} />
                     </div>
                     </div>
                     <PlaySelected image={player1Play} />
                 </div>
                 <div>
+                    <h3 className='score'>SCORE</h3>
                     <h3 className='score'>{player1Score}-{player2Score}</h3>
+                    <button className='btn-pa' onClick={() => resetValues()}>PLAY AGAIN</button>
                     <h3 className='winner'>{result}</h3>
                 </div>
                 <div className='rivalOptions'>
                 <PlaySelected image={comPlay} />
                     <div className='b'>
-                    <h1>Choose your play!</h1>
+                    <h1>COM</h1>
                     <div className='images'>
-                    <img src={lizard} alt="lizard"  />
-                    <img src={paper} alt="lizard" />
-                    <img src={rock} alt="lizard" />
-                    <img src={scissors} alt="lizard" />
-                    <img src={spock} alt="lizard" />
+                            <img src={lizard} alt="lizard"  />
+                            <img src={paper} alt="paper" />
+                            <img src={rock} alt="rock" />
+                            <img src={scissors} alt="scissors" />
+                            <img src={spock} alt="spock" />
                     </div>
                     </div>
                 </div>
                 </div>
                 
             <div>
-                <h3>YOUR PLAY: {player1Play}</h3>
-                <h3>COM PLAY: {comPlay}</h3>
+               
                 
                 
 
             </div>
             </div>
+)
+
+
+}
+
+      const againstPlayer2= () =>{
+        return(
+            <div className='videogame' >
+                        
+                        
+                        <div className='versus'>
+                        <div className='yourOptions'>
+                            <div className='a'>
+                                <h1>Player 1</h1>
+                                    <div className='images' style={{  
+            pointerEvents: player1Play != null ? 'none' : 'auto',
+        }}>
+                                <img src={lizard} alt="lizard" onClick={() => {handleClickPVP("lizard", true)}} />
+                                <img src={paper} alt="paper" onClick={() => {handleClickPVP("paper", true)}} />
+                                <img src={rock} alt="rock" onClick={() => {handleClickPVP("rock", true)}}/>
+                                <img src={scissors} alt="scissors" onClick={() => {handleClickPVP("scissors", true)}} />
+                                <img src={spock} alt="spock" onClick={() => {handleClickPVP("spock", true)}} />
+                            </div>
+                            </div>
+                            <div class="flip-box">
+                                    <div class="flip-box-inner">
+                                        <div class="flip-box-front">
+                                        <img src={lizard} alt="Paris" style={{width: '300px',height:'200px'}} />
+                                        </div>
+                                        <div class="flip-box-back">
+                                        <h2 className='text-back'>PLAYER 1 SELECT</h2>
+                                        
+                                        </div>
+                                    </div>
+                                </div>
+                            <PlaySelected image={player1Play} />
+                        </div>
+                        <div>
+                            <h3 className='score'>SCORE</h3>
+                            <h3 className='score'>{player1Score}-{player2Score}</h3>
+                            <button className='btn-pa' onClick={() => resetValues()}>PLAY AGAIN</button>
+                            <h3 className='winner'>{result}</h3>
+                        </div>
+                        <div className='rivalOptions'>
+                        <PlaySelected image={comPlay} />
+                            <div className='b'>
+                            <h1>Player 2</h1>
+                            <div className='images'style={{  
+            pointerEvents: player1Play != null && comPlay == null ? 'auto' : 'none',
+        }}>
+                            <img src={lizard} alt="lizard" onClick={() => {handleClickPVP("lizard", false)}} />
+                        <img src={paper} alt="paper" onClick={() => {handleClickPVP("paper", false)}} />
+                        <img src={rock} alt="rock" onClick={() => {handleClickPVP("rock",false)}}/>
+                        <img src={scissors} alt="scissors" onClick={() => {handleClickPVP("scissors",false)}} />
+                        <img src={spock} alt="spock" onClick={() => {handleClickPVP("spock",false )}} />
+                          
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        
+                    <div>
+                       
+                        
+                        
+        
+                    </div>
+                    </div>
+        )
+        
+              }
+
+     const ActualGame= gameMode === "vsCOM" && isPlaying ? againstComputer : (gameMode === "vsP2" ? againstPlayer2 : "ss")  
+
+      
+  return (
+    <div>
+            <div>
+                <h3 className='vsCom' onClick={() => {setGameMode("vsCOM"); setIsPlaying(true);}}>VS COM</h3>
+                <h3 onClick={() => {setGameMode("vsP2"); setIsPlaying(true);}}>VS P2</h3>
+            </div>
+            <ActualGame style={{  
+                        display: isPlaying ? 'contents' : 'none',
+      }}/>
+            
            
             
 
